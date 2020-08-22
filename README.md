cda9fe5d6f2f1e7c1043f2953ebf341fae6ba67d
TheOllieJT <contact@olliejt.com>
2020-08-22 11:52:00 +0200

    Updated Config

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 # routify-starter
 
-Starter template for [Routify](https://github.com/sveltech/routify)
+A Svelte project using [Routify](https://github.com/sveltech/routify) to manage routes, SSR, Service Worker and more...
 
-### Get started
+## Author
 
-To use this starter run `npx @sveltech/routify init` in an empty folder.
+Project lead dev: Ollie Taylor ([OllieJT](https://github.com/OllieJT)) ==> [contact@olliejt.com](mailto:contact@olliejt.com)
 
-Alternatively, you can clone this repo.
+## Get started
 
-### Npm scripts
+### npm scripts
 
-| Syntax           | Description                                                                       |
-|------------------|-----------------------------------------------------------------------------------|
-| `dev`            | Development (port 5000)                                                           |
-| `dev-dynamic`    | Development with dynamic imports                                                  |
-| `build`          | Build a bundled app with SSR + prerendering and dynamic imports                   |
-| `serve`          | Run after a build to preview. Serves SPA on 5000 and SSR on 5005                  |
-| `deploy:*`       | Deploy to netlify or now                                                          |
-| `export`         | Create static pages from content in dist folder (used by `npm run build`)         |
+| Syntax        | Description                                                               |
+| ------------- | ------------------------------------------------------------------------- |
+| `dev`         | Development (port 5000)                                                   |
+| `dev:nollup`  | Development with crazy fast rebuilds (port 5000)                          |
+| `dev:dynamic` | Development with dynamic imports                                          |
+| `build`       | Build a bundled app with SSR + prerendering and dynamic imports           |
+| `serve`       | Run after a build to preview. Serves SPA on 5000 and SSR on 5005          |
+| `deploy:*`    | Deploy to netlify or now                                                  |
+| `export`      | Create static pages from content in dist folder (used by `npm run build`) |
+| `format`      | Format all files in /src with prettier                                    |
 
 ### SSR and pre-rendering
 
@@ -27,7 +29,7 @@ SSR and pre-rendering are included in the default build process.
 
 To render async data, call the `$ready()` helper whenever your data is ready.
 
-If $ready() is present, rendering will be delayed till the function has been called.
+If \$ready() is present, rendering will be delayed till the function has been called.
 
 Otherwise it will be rendered instantly.
 
@@ -35,10 +37,6 @@ See [src/pages/example/api/[showId].svelte](https://github.com/sveltech/routify-
 
 ### Production
 
-* For SPA or SSR apps please make sure that url rewrite is enabled on the server.
-* For SPA redirect to `__dynamic.html`.
-* For SSR redirect to the lambda function or express server.
-
-### Issues?
-
-File on Github! See https://github.com/sveltech/routify/issues .
+-   For SPA or SSR apps please make sure that url rewrite is enabled on the server.
+-   For SPA redirect to `__dynamic.html`.
+-   For SSR redirect to the lambda function or express server.
--- rollup.config.js
+++ rollup.config.js
@@ -1,147 +1,37 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import copy from 'rollup-plugin-copy';
-import del from 'del';
-
-const staticDir = 'static';
-const distDir = 'dist';
-const buildDir = `${distDir}/build`;
-const production = !process.env.ROLLUP_WATCH;
-const bundling = process.env.BUNDLING || production ? 'dynamic' : 'bundle';
-const shouldPrerender =
-	typeof process.env.PRERENDER !== 'undefined' ? process.env.PRERENDER : !!production;
+import { createRollupConfigs } from './scripts/base.config.js'
 
 const preprocess = require('./svelte.config.js');
-console.log(`${buildDir}/bundle.css`);
 
-del.sync(distDir + '/**');
+const production = !process.env.ROLLUP_WATCH;
 
-function createConfig({ output, inlineDynamicImports, plugins = [] }) {
-	const transform = inlineDynamicImports ? bundledTransform : dynamicTransform;
-
-	return {
-		inlineDynamicImports,
-		input: `src/main.js`,
-		output: {
-			name: 'app',
-			sourcemap: true,
-			...output,
-		},
-		plugins: [
-			copy({
-				targets: [
-					{ src: staticDir + '/**/!(__index.html)', dest: distDir },
-					{
-						src: `${staticDir}/__index.html`,
-						dest: distDir,
-						rename: '__app.html',
-						transform,
-					},
-				],
-				copyOnce: true,
-				flatten: false,
-			}),
-			svelte(preprocess),
-
-			// If you have external dependencies installed from
-			// npm, you'll most likely need these plugins. In
-			// some cases you'll need additional configuration —
-			// consult the documentation for details:
-			// https://github.com/rollup/rollup-plugin-commonjs
-			resolve({
-				browser: true,
-				dedupe: (importee) => importee === 'svelte' || importee.startsWith('svelte/'),
-			}),
-			commonjs(),
-
-			// If we're building for production (npm run build
-			// instead of npm run dev), minify
-			production && terser(),
-
-			...plugins,
-		],
-		watch: {
-			clearScreen: false,
-		},
-	};
+export const config = {
+  staticDir: 'static',
+  distDir: 'dist',
+  buildDir: `dist/build`,
+  serve: !production,
+  production,
+  rollupWrapper: rollup => rollup,
+  svelteWrapper: svelte => {
+    svelte.preprocess = [preprocess]
+  },
+  swWrapper: worker => worker,
 }
 
-/* === === ===
-  If bundling, load alternate config
-=== === === */
-const bundledConfig = {
-	inlineDynamicImports: true,
-	output: {
-		format: 'iife',
-		file: `${buildDir}/bundle.js`,
-	},
-	plugins: [!production && serve(), !production && livereload(distDir)],
-};
+const configs = createRollupConfigs(config)
 
-const dynamicConfig = {
-	inlineDynamicImports: false,
-	output: {
-		format: 'esm',
-		dir: buildDir,
-	},
-	plugins: [!production && livereload(distDir)],
-};
-const configs = [createConfig(bundledConfig)];
-if (bundling === 'dynamic') configs.push(createConfig(dynamicConfig));
-if (shouldPrerender) [...configs].pop().plugins.push(prerender());
-export default configs;
+export default configs
 
-/* === === ===
-  UTILITY
-=== === === */
-function serve() {
-	let started = false;
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
-				require('child_process').spawn('npm', ['run', 'serve'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true,
-				});
-			}
-		},
-	};
-}
+/**
+  Wrappers can either mutate or return a config
 
-function prerender() {
-	return {
-		writeBundle() {
-			if (shouldPrerender) {
-				require('child_process').spawn('npm', ['run', 'export'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true,
-				});
-			}
-		},
-	};
-}
+  wrapper example 1
+  svelteWrapper: (cfg, ctx) => {
+    cfg.preprocess: mdsvex({ extension: '.md' }),
+  }
 
-function bundledTransform(contents) {
-	return contents.toString().replace(
-		'__SCRIPT__',
-		`
-	<script defer src="/build/bundle.js" ></script>
-	`,
-	);
-}
-
-function dynamicTransform(contents) {
-	return contents.toString().replace(
-		'__SCRIPT__',
-		`
-  <script type="module" defer src="https://unpkg.com/dimport@1.0.0/dist/index.mjs?module" data-main="/build/main.js">
-  </script>
-  <script nomodule defer src="https://unpkg.com/dimport/nomodule" data-main="/build/main.js">
-  </script>
-	`,
-	);
-}
+  wrapper example 2
+  rollupWrapper: cfg => {
+    cfg.plugins = [...cfg.plugins, myPlugin()]
+    return cfg
+  }
+*/
