cc810017329edda01c9de8f3f8842b734a3024a5
TheOllieJT <contact@olliejt.com>
2020-08-22 12:09:08 +0200

    Update Routify

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script>
-	import { Router } from '@sveltech/routify';
-	import { routes } from '@sveltech/routify/tmp/routes';
-
+	import { Router } from "@sveltech/routify";
+	import { routes } from "../.routify/routes";
 </script>
+
+<style global lang="scss">
+	@import "./main.scss";
+</style>
 
 <Router {routes} />
