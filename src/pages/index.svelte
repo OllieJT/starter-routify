3ad92e21595b7bb08adfa6b459f16727bdc5e2d5
TheOllieJT <contact@olliejt.com>
2020-08-22 12:10:43 +0200

    Demo Content

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,5 +1,126 @@
 <script>
-    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
+	import { metatags } from "@sveltech/routify";
+	metatags.title = "Dignitas";
+	metatags.description = "Description coming soon...";
 </script>
 
-<RoutifyIntro />
+<article>
+	<h1>
+		DIGNITAS AND VIE.GG EXPAND EXISTING PARTNERSHIP WITH A FOCUS ON THE NEW JERSEY
+		MARKETPLACE
+	</h1>
+	<h3>CS:GO TEAM WILL COMPETE AS DIGNITASVIE IN OFFICIAL EVENTS</h3>
+
+	<h3>
+		PARTNERSHIP ACTIVATIONS TO UTILIZE DIGNITAS’ NEW JERSEY BASED ESPORTS TRAINING
+		FACILITY
+	</h3>
+	<p style="text-align: left;">
+		<strong>NEWARK, NJ – AUGUST 17, 2020</strong>
+		: Dignitas, the esports vertical of New Meta Entertainment Inc. (NME), has today
+		announced an expanded partnership with Esports Entertainment Group’s VIE.gg. The
+		online exchange betting platform, VIE.gg, became an official multi-year partner
+		of Dignitas in June 2019 and in April 2020 became the first publicly traded
+		esports wagering company, listing its common shares on the NASDAQ, under the
+		symbol “GMBL”. Today marks an expansion of that partnership as VIE.gg secures
+		the naming rights of the organization’s famed CS:GO team.
+		<br />
+		<img
+			style="display: block; margin-left: auto; margin-right: auto;"
+			src="https://d1mt9jmphk9kik.cloudfront.net/teamdignitas/image1597636515.png"
+			width="519"
+			height="292"
+		/>
+	</p>
+	<p>
+		VIE.gg is the first and most transparent esports bet exchange as a result of
+		Esports Entertainment Group being a fully reporting SEC issuer in the United
+		States. Player safety features built into VIE.gg create a fun but responsible
+		esports betting experience for fans.
+	</p>
+	<p>
+		In addition to VIE.gg’s integration into the CS:GO naming rights and jersey, the
+		partners will roll out strategic digital and physical activations with the New
+		Jersey marketplace as a core focus. In June 2018, New Jersey Governor Phil
+		Murphy signed Assembly Bill 411, legalizing sports betting in New Jersey. One
+		year later, Dignitas became the first esports organization to open an esports
+		training facility in Newark, New Jersey, next to the famed sports and
+		entertainment facility, Prudential Center. In addition to U.S. sports betting,
+		the partnership also paves the way into the European marketplace through
+		association with Dignitas’ famed CS:GO team hailing from Scandinavia.
+	</p>
+	<p>
+		Esports Entertainment Group, a member of ESIC, is working together with NME, to
+		ensure that conflicts of interest and betting and age requirement rules are
+		withheld throughout.
+		<br />
+		<br />
+	</p>
+	<center>
+		<iframe
+			src="https://www.youtube.com/embed/CDGG_RFCdTg"
+			width="560"
+			height="315"
+			frameborder="0"
+			allowfullscreen="allowfullscreen"
+		/>
+	</center>
+	<p>
+		<br />
+		“We are honored to partner with an innovative, multinational brand like VIE.gg,”
+		said Michael Prindiville, CEO of New Meta Entertainment and Dignitas. “Our
+		co-branded CS:GO team and the immense opportunity around esports betting content
+		in our home state of New Jersey enable us to tell unique and engaging stories at
+		the forefront of both esports betting and competition to millions of fans
+		worldwide.”
+		<br />
+		<br />
+		“We are excited to further expand our partnership with Dignitas,” commented
+		Magnus Leppäniemi, VP of Marketing and Head of Esports at Esports Entertainment
+		Group. “As a committed long-term partner, we’re thrilled to have the opportunity
+		to co-brand our VIE.gg platform alongside Dignitas’ legendary CS:GO team, an
+		opportunity that can introduce our brand to millions of esports fans globally
+		and help establish our status as a Tier 1 operator within esports betting.”
+	</p>
+	<h4>ABOUT DIGNITAS:</h4>
+
+	<p>
+		Since its formation in 2003, Dignitas has established itself as one of the most
+		successful esports organizations in the world, amassing 18 World Championships
+		across multiple game titles. In September 2016, Dignitas was acquired by the
+		NBA’s Philadelphia 76ers of Harris Blitzer Sports &amp; Entertainment (HBSE), a
+		diverse, global portfolio of sports and entertainment franchises and properties
+		that includes the Philadelphia 76ers (NBA), New Jersey Devils (NHL) and more. In
+		June 2019, Dignitas merged with Fertitta Entertainment’s League of Legends LCS
+		franchise, Clutch Gaming, and through investment with Susquehanna International
+		Group and Delaware North, formed the parent company New Meta entertainment, Inc.
+		(NME), to expand the esports business into a broader digital sports and
+		entertainment company. Dignitas currently fields seven teams in five of esports'
+		largest and most popular games: League of Legends, Counter-Strike: Global
+		Offensive (men’s and women’s teams), Super Smash Bros., Rocket League, and Clash
+		Royale – as well as a host of digital influencers. With player content studios
+		and wellness facilities in Newark, NJ and Los Angeles, and infrastructure in
+		Europe and China, Dignitas is the global leader dedicated to esports athletes
+		and gaming entertainers.
+	</p>
+
+	<h4>ABOUT ESPORTS ENTERTAINMENT GROUP:</h4>
+
+	<p>
+		Esports Entertainment Group, Inc. is a licensed online gambling company with a
+		specific focus on esports wagering and 18+ gaming. The Company holds a license
+		to conduct online gambling and 18+ gaming on a global basis in Malta and
+		Curacao, Kingdom of the Netherlands and is able to accept wagers from over 149
+		jurisdictions including Canada, Japan, Germany and South Africa. Esports
+		Entertainment offers fantasy, pools, fixed odds and exchange style wagering on
+		esports events in a licensed, regulated and secure platform to the global
+		esports audience at vie.gg. In addition, Esports Entertainment intends to offer
+		users from around the world the ability to participate in multi-player mobile
+		and PC video game tournaments for cash prizes. Esports Entertainment is led by a
+		team of industry professionals and technical experts from the online gambling
+		and the video game industries, and esports. The Company maintains offices in
+		Malta. For more information visit
+		<a href=".">www.esportsentertainmentgroup.com</a>
+		.
+	</p>
+</article>
